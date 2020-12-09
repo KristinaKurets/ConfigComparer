83a4c649a0d62184b68105ecd6777c68e80d102c
DzmitryMikhailau <dzmitry.mikhailau@p97.com>
2020-12-09 14:50:11 +0300

    console info added

--- ConfigComparer/Program.cs
+++ ConfigComparer/Program.cs
@@ -10,26 +10,61 @@ namespace ConfigComparer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello! This is a program for config files comparison. Please, read the 'README.txt' file before starting.");
+            Console.WriteLine("Have you read it already? Press y/n");
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                if (userInput == "n")
+                {
+                    Console.WriteLine("Please, read and come back! Press any key to close the program.");
+                    Console.ReadKey();
+                    return;
+                }
+                else if (userInput == "y")
+                {
+                    Console.WriteLine("Great, let's start.");
+                    var cloudProjectName = ConfigurationManager.AppSettings["CloudProjectPath"];
+                    var loyaltyProjectName = ConfigurationManager.AppSettings["LoyaltyProjectPath"];
+                    var searchFile = ConfigurationManager.AppSettings["SearchFile"];
+                    var loggerPath = ConfigurationManager.AppSettings["LoggerPath"];
+                    var serializer = new Serializer.Serializer();
+                    var logger = new Logger.Logger(loggerPath);
+                    var fileParser = new FileParser(serializer);
+                    Console.WriteLine($"Are you sure you want to compare the config files in folders {cloudProjectName} and {loyaltyProjectName}? Press y/n");
+                    while (true)
+                    {
+                        userInput = Console.ReadLine();
+                        if (userInput == "n")
+                        {
+                            Console.WriteLine("Please, change folders paths in 'ConfigComparer.dll.config' and come back. Press any key to close the program.");
+                            Console.ReadKey();
+                            return;
+                        }
+                        else if (userInput == "y")
+                        {
+                            {
+                                var comparer = new FilesComparer(logger, fileParser);
+                                try
+                                {
+                                    comparer.GoCompare(cloudProjectName, loyaltyProjectName, searchFile, loggerPath);
+                                }
+                                catch (Exception ex)
+                                {
+                                    logger.LogException(ex);
+                                }
+                                Console.WriteLine($"Well done. You can find comparison result in {loggerPath} file. Press any key to close the program.");
+                                Console.ReadKey();
+                                return;
+                            }
+                        }
+                        else Console.WriteLine("Wrong input! Press y/n");
+                    }
+                    
+                }
+                else Console.WriteLine("Wrong input! Press y/n");
+            }
             
-            var cloudProjectName = ConfigurationManager.AppSettings["CloudProjectPath"];
-            var loyaltyProjectName = ConfigurationManager.AppSettings["LoyaltyProjectPath"];
-            var searchFile = ConfigurationManager.AppSettings["SearchFile"];
-            var loggerPath = ConfigurationManager.AppSettings["LoggerPath"];
-            var serializer = new Serializer.Serializer();
-            var logger = new Logger.Logger(loggerPath);
-            var fileParser = new FileParser(serializer);
-            var comparer = new FilesComparer(logger, fileParser);
-
-            try
-            {
-                comparer.GoCompare(cloudProjectName, loyaltyProjectName, searchFile, loggerPath);
-            }
-            catch (Exception ex)
-            {
-                logger.LogException(ex);
-            }
-            Console.WriteLine("Well done.");
-            Console.ReadKey();
         }
     }
 }
--- README.txt
+++ README.txt
@@ -1,12 +1,13 @@
 Hello! This is a comparer for XML config files.
-If you want to find keys, that have different values in different config files, this program will be useful for you :) 
+Specify 2 folders in which configuration files can be located. The program compares ALL configuration files and looks for records with the same settings, but different values, and outputs them to the log file.
+
 Lets's start!
 
-1. Open the ConfigComparer.sln file.
-2. Then find and open app.config file and change keys 'CloudProjectPath'and 'LoyaltyProjectPath' with your projects paths.
-3. Also you can change search file and path for your logging file in keys 'SearchFile' and 'LoggerPath', respectively.
-4. Now, after all your changes, run the project.
-5. Result of your comparing you can see in the logging file.
+1. Open 'ConfigComparer.dll.config' file and change keys 'CloudProjectPath'and 'LoyaltyProjectPath' with your projects paths
+   (you can specify the root folder - the program looks for files needed for comparison in all subfolders).
+2. Also you can change search file and path for your logging file in keys 'SearchFile' and 'LoggerPath', respectively.
+3. Now, after all your changes, run 'ConfigComparer.exe'.
+4. Result of your comparing you can see in the logging file.
 
 That's all!
 Enjoy :)
